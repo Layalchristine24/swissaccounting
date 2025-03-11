11f192ccba74d0cb493bede6952098000429ea10
Layal Christine Lettry <layal.lettry@gmail.com>
2025-03-11 08:52:46 +0100

    feat: rewrite sum_accounts

--- R/sum_accounts.R
+++ R/sum_accounts.R
@@ -1,18 +1,19 @@
 sum_accounts <- function(my_ledger) {
   my_ledger |>
-    get_high_category() |>
-    get_intermediate_category() |> 
-    reframe(
+    filter(!(account_type %in%
+      c("Income/Expense", "Closing", "Produit/Charge", "Cl\u00f4ture", "Einnahmen/Ausgabe", "Abschluss"))) |>
+    mutate(
       amount = case_when(
-        high_category == 1e1 &
+        account_type %in% c("Asset", "Expense", "Actif", "Charge", "Aktivkonto", "Ausgabe") &
           !is.na(debit_account) ~ amount,
+        account_type %in% c("Liability", "Income", "Passif", "Produit", "Passivkonto", "Einnahmen") &
+          !is.na(credit_account) ~ amount,
         .default = -amount
-      ),
-      .by = c(date, id, account_description, debit_account, credit_account)
+      )
     ) |>
+    get_high_category() |>
     reframe(
       sum_assets = sum(amount, na.rm = TRUE),
-      .by = account_description
+      .by = c(high_category, account_description)
     )
 }
-ledger_file <- "/Users/Layal/github/ersatz-production/ersatz-accounting/ersatz.accounting/documents/ledger/ersatz-ledger.csv"
