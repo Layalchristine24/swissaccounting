2f8f32ce115463d38fb5d85fa409a0cf1064f1bf
Layal Christine Lettry <layal.lettry@gmail.com>
2025-04-04 10:23:02 +0200

    docs: update globals

--- R/globals.R
+++ R/globals.R
@@ -25,5 +25,6 @@ utils::globalVariables(c(
   "account_number", # <get_balance_category>
   "account_base_category", # <get_balance_category>
   "intermediate_category", # <get_balance_category>
+  "sum_amounts", # <write_balance_sheet>
   NULL
 ))
