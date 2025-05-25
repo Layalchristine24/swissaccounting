38234a461235ca01efd6ae28cadbce9734e37532
Layal Christine Lettry <layal.lettry@gmail.com>
2025-05-25 09:05:28 +0200

    fix: remove path_csv argument from get_income_statement

--- R/get_income_statement.R
+++ R/get_income_statement.R
@@ -12,8 +12,6 @@
 #'   statement period (format: "YYYY-MM-DD")
 #' @param max_date character,Date Optional. End date for the income
 #'   statement period (format: "YYYY-MM-DD")
-#' @param path_csv character Optional. Path where to save the income statement CSV
-#'   file. If provided, the function will write the results to this file.
 #' @param language character Language code for account descriptions. One of
 #'   "en", "fr", "de". Defaults to "fr"
 #'
@@ -37,15 +35,6 @@
 #'   max_date = "2024-12-31",
 #'   language = "fr"
 #' )
-#'
-#' # Generate and save to CSV
-#' get_income_statement(
-#'   ledger_file = "path/to/ledger.csv",
-#'   min_date = "2024-01-01",
-#'   max_date = "2024-12-31",
-#'   path_csv = "path/to/income_statement.csv",
-#'   language = "fr"
-#' )
 #' }
 #'
 #' @seealso
@@ -58,7 +47,6 @@ get_income_statement <- function(
     ledger_file,
     min_date,
     max_date,
-    path_csv,
     language = "fr") {
   income <- get_category_total(
     ledger_file = ledger_file,
